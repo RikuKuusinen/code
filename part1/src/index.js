6645164df5174182738ad68ccb6c17fa361f3cdd
RikuKuusinen <rikuttaja91@gmail.com>
2020-07-17 21:23:00 +0300

    part 1.5

--- part1/src/index.js
+++ part1/src/index.js
@@ -2,25 +2,29 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
-
-
-  var xdd = {part:part1, exercises: exercises1};
-  var xd1 = {part:part2, exercises: exercises2};
-  var xd2 = {part:part3, exercises: exercises3};
-var xd = [xdd, xd1, xd2];
+  const course = {
+    name: 'Half Stack application development',
+    parts:  [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
+}
 
   return (
     <div>
-      <Header course={course}></Header>
-      <Content xd={xd}></Content>
-      <Total count={exercises1 + exercises2 + exercises3}></Total>
+      <Header course={course.name}></Header>
+      <Content parts={course.parts}></Content>
+      <Total count={course.parts.length}></Total>
     </div>
   )
 }
@@ -32,12 +36,8 @@ const Header = (props) => {
 
 }
 const Content = (props)=> {
-var xd = props.xd;
-var xxdd = xd.map(element => {
-});
-
-var map = props.xd.map(element => {
- return <Part part={element.part} exercises={element.exercises}></Part>
+var map = props.parts.map(element => {
+ return <Part part={element.name} exercises={element.exercises}></Part>
 });
 return map;
 }
